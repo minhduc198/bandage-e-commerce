a8bd6993d85988e92c46eae9eecfce22ec8e1af0
nguyenminhduc1982005@gmail.com <nguyenminhduc1982005@gmail.com>
2024-09-29 17:28:46 +0700

    Feature products

--- dist/index.js
+++ dist/index.js
@@ -1,8 +1,9 @@
 import btn from "./models/button.js";
+import carousel from "./models/carousel.js";
 import menu from "./models/menu.js";
 import video from "./models/video.js";
 menu.openMenu();
 menu.closeMenu();
-// carousel.onShowSlide();
+carousel.onShowSlide();
 video.togglePlayBtn();
 btn.onclickBtn();
